ccdf51b4457d248d3f216da187409e4e42642009
kris <kris.raich@gmail.com>
2020-10-14 17:06:09 +0200

    init branch

--- src/main/java/at/taaja/purpletiger/GeoCodingResource.java
+++ src/main/java/at/taaja/purpletiger/GeoCodingResource.java
@@ -2,19 +2,21 @@ package at.taaja.purpletiger;
 
 
 import com.fasterxml.jackson.annotation.JsonView;
+import io.quarkus.runtime.StartupEvent;
 import io.taaja.models.generic.LocationInformation;
-import io.taaja.models.record.spatial.Area;
-import io.taaja.models.record.spatial.LongLat;
-import io.taaja.models.record.spatial.SpatialEntity;
+import io.taaja.models.record.spatial.*;
 import io.taaja.models.views.SpatialRecordView;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.geom.Polygon;
 
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Path("/v1")
@@ -22,6 +24,12 @@ public class GeoCodingResource {
 
     @Inject
     ExtensionRepository extensionRepository;
+
+    private GeometryFactory geometryFactory;
+
+    void onStart(@Observes StartupEvent ev) {
+        this.geometryFactory = new GeometryFactory();
+    }
 
 
     @GET
@@ -48,12 +56,15 @@ public class GeoCodingResource {
 
         for (SpatialEntity currentSpatialEntity : this.extensionRepository.findAll()) {
             currentArea = (Area) currentSpatialEntity;
-            Coordinate[] currentCoords = toGeoCoords(currentArea);
+            Coordinate[] currentCoords = getCoordinatesFromSpatialEntity(currentArea);
             currentElevation = currentArea.getElevation();
             currentHeight = currentArea.getHeight();
 
-            if (altitude == null) {
-                if (altitude < currentElevation || altitude > currentElevation + currentHeight) continue;
+            if (
+                    altitude != null &&
+                    (altitude < currentElevation || altitude > currentElevation + currentHeight)
+            ){
+                continue;
             }
 
             if (currentCoords.length >= 4) {
@@ -83,47 +94,98 @@ public class GeoCodingResource {
     @Path("/calculate/intersectingExtensions")
     @JsonView({SpatialRecordView.Identity.class})
     @Consumes(MediaType.APPLICATION_JSON)
-    public LocationInformation getAffectedAreas(SpatialEntity spatialEntity) {
+    public LocationInformation getAffectedSpatialEntities(SpatialEntity spatialEntity) {
         LocationInformation li = new LocationInformation();
-        GeometryFactory gf = new GeometryFactory();
 
-        Area area = (Area) spatialEntity;
-        Coordinate[] coords = toGeoCoords(area);
-        Polygon poly1 = gf.createPolygon(coords);
 
-        Area currentArea;
-        float elevation = area.getElevation();
-        float currentElevation;
-        float height = area.getHeight();
-        float currentHeight;
+
+        Coordinate[] coords = getCoordinatesFromSpatialEntity(spatialEntity);
+
+        if(coords == null){
+            throw new WebApplicationException("Cant retrieve Coordinates from SpatialEntity");
+        }
+
+        Polygon polygonToCheck = gf.createPolygon(coords);
+
 
         for (SpatialEntity currentSpatialEntity : this.extensionRepository.findAll()) {
-            currentArea = (Area) currentSpatialEntity;
-            Coordinate[] currentCoords = toGeoCoords(currentArea);
-            currentElevation = currentArea.getElevation();
-            currentHeight = currentArea.getHeight();
 
-            //check if Areas overlap vertical
-            if (elevation + height < currentElevation || elevation > currentElevation + currentHeight) continue;
+            if(spatialEntity instanceof Area) {
+                Area area = (Area) spatialEntity;
 
-            //check if Entity is Polygon (Error below 4 points)
-            if (currentCoords.length >= 4) {
+                float elevation = area.getElevation();
+                float currentElevation;
+                float height = area.getHeight();
+                float currentHeight;
+
+
+                Area currentArea = (Area) currentSpatialEntity;
+                Coordinate[] currentCoords = getCoordinatesFromSpatialEntity(currentArea);
+
+                if(currentCoords == null){
+                    continue;
+                }
+
+                currentElevation = currentArea.getElevation();
+                currentHeight = currentArea.getHeight();
+
+                //check if Areas overlap vertical
+                if (elevation + height < currentElevation || elevation > currentElevation + currentHeight) continue;
+
+                //check if Entity is Polygon (Error below 4 points)
                 Polygon poly2 = gf.createPolygon(currentCoords);
-                if (poly1.overlaps(poly2) || poly1.within(poly2) || poly2.within(poly1)) {
+                if (polygonToCheck.overlaps(poly2) || polygonToCheck.within(poly2) || poly2.within(polygonToCheck)) {
                     li.addSpatialEntity(currentSpatialEntity);
                 }
+
+
+            }else {
+                Corridor corridor = (Corridor) spatialEntity;
+
+
+
             }
+
+
+
+
+
         }
         return li;
     }
 
-    private static Coordinate[] toGeoCoords(Area area) {
-        Coordinate[] coords = new Coordinate[area.getCoordinates().get(0).size()];
-        int i = 0;
-        for (LongLat ll : area.getCoordinates().get(0)) {
-            coords[i] = new Coordinate(ll.getLongitude(), ll.getLatitude());
-            i++;
+    private Polygon getCoordinatesFromSpatialEntity(SpatialEntity spatialEntity) {
+
+        ArrayList<Coordinate> out = new ArrayList();
+
+        if(spatialEntity instanceof Area){
+            Area area = (Area) spatialEntity;
+
+            if(area.getCoordinates() == null || area.getCoordinates().isEmpty() || area.getCoordinates().get(0).size() < 4){
+                return null;
+            }
+
+            List<LongLat> longLats = area.getCoordinates().get(0);
+            longLats.forEach(longLat -> out.add(new Coordinate(longLat.getLongitude(), longLat.getLatitude())));
+
+        }else {
+
+            // create polygon or LineString from Corridor
+            Corridor corridor = (Corridor) spatialEntity;
+
+            if(corridor.getCoordinates() == null || corridor.getCoordinates().isEmpty()){
+                return null;
+            }
+
+            List<Waypoint> waypoints = corridor.getCoordinates();
+            waypoints.forEach(waypoint -> out.add(new Coordinate(waypoint.getLongitude(), waypoint.getLatitude())));
         }
-        return coords;
+
+
+        return this.geometryFactory.createPolygon(
+                out.toArray(Coordinate[]::new)
+        );
     }
+
+
 }
